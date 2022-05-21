eab221bbe9e2924c8ad422f30fff921d17898625
iamgamja <hmmthinking@naver.com>
2022-05-21 15:00:11 +0900

    Add Files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <div class="left">
+      <canvas></canvas>
+    </div>
+    <div class="right">
+      <Setting-component v-for="i in 5" v-bind:key="i"></Setting-component>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import SettingComponent from './components/Setting-component.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    components: {
+      SettingComponent
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  /* Desktop */
+  .main {
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+    background: #edf4ff;
+  }
+
+  div.left {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vh;
+    height: 100%;
+  }
+
+  div.left > canvas {
+    background: white;
+    border: 1px black solid;
+  }
+
+  div.right {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex: 1;
+    height: 100%;
+    background: #bfd3f3;
+  }
+
+  /* Mobile */
+  @media ( max-aspect-ratio: 1/1 ) {
+    .main {
+      flex-direction: column;
+    }
+
+    div.left {
+      width: initial;
+      height: 100vw;
+    }
+  }
 </style>
---
+++ src/components/Setting-component.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="setting">
+    hi! I'm Setting!
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Setting-component'
+  }
+</script>
+
+<style scoped>
+  .setting {
+    width: 90%;
+    margin: 5px;
+    padding: 5px;
+    box-sizing: border-box;
+    border-radius: 5px;
+    background: white;
+  }
+</style>
